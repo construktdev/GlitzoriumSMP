db3e0592be4f130e8e18b858d29606ea8c062d33
construktdev <construktai@gmail.com>
2024-11-28 21:51:25 +0100

    Add tab completion for status command
    Implemented tab completion to suggest available status options when using the /status command. This enhancement improves user experience by making it easier to select and use status prefixes.

--- src/main/java/de/construkter/glitzoriumSMP/GlitzoriumSMP.java
+++ src/main/java/de/construkter/glitzoriumSMP/GlitzoriumSMP.java
@@ -70,6 +70,7 @@ public final class GlitzoriumSMP extends JavaPlugin {
         Objects.requireNonNull(getCommand("mute")).setExecutor(new MuteCommand());
         Objects.requireNonNull(getCommand("unmute")).setExecutor(new UnmuteCommand());
         Objects.requireNonNull(getCommand("status")).setExecutor(new StatusCommand());
+        Objects.requireNonNull(getCommand("status")).setTabCompleter(new StatusCommand());
         Objects.requireNonNull(getCommand("clearwarns")).setExecutor(new ClearWarnsCommand());
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         PrepareStartCommand.isStarted = true; // Default is true, will be turned false when the prepare command is executed
--- src/main/java/de/construkter/glitzoriumSMP/tablist/StatusCommand.java
+++ src/main/java/de/construkter/glitzoriumSMP/tablist/StatusCommand.java
@@ -1,62 +1,88 @@
 package de.construkter.glitzoriumSMP.tablist;
 
-import de.construkter.glitzoriumSMP.utils.Prefix;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class StatusCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StatusCommand implements CommandExecutor, TabCompleter {
+
+    // Liste der verfügbaren Status-Prefixe
+    private static final List<String> STATUS_OPTIONS = Arrays.asList(
+            "ghg", "bambus", "redstone", "live", "afk", "pvp", "builder", "troll", "polizei", "list"
+    );
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if (!(commandSender instanceof Player player)){
+        if (!(commandSender instanceof Player player)) {
             commandSender.sendMessage("You must be a player to use this command!");
             return true;
         }
         if (strings.length != 1) {
             commandSender.sendMessage(ChatColor.RED + "Usage: /status <status>");
-            commandSender.sendMessage(Prefix.PluginPrefix() + ChatColor.DARK_AQUA + "Nutze /status list um dir alle möglichen Status Prefixes anzuzeigen");
+            commandSender.sendMessage(ChatColor.DARK_AQUA + "Nutze /status list um dir alle möglichen Status Prefixes anzuzeigen");
             return true;
         }
         switch (strings[0].toLowerCase()) {
             case "ghg":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&5GHG&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&5GHG&7] ");
                 break;
             case "bambus":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&2Bambus&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&2Bambus&7] ");
                 break;
             case "redstone":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&4Redstone&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&4Redstone&7] ");
                 break;
             case "live":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&eLive&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&eLive&7] ");
                 break;
             case "afk":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&8AFK&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&8AFK&7] ");
                 break;
             case "pvp":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&aPvP&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&aPvP&7] ");
                 break;
             case "builder":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&3Builder&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&3Builder&7] ");
                 break;
             case "troll":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&6Troll&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&6Troll&7] ");
                 break;
             case "polizei":
-                de.construkter.glitzoriumSMP.tablist.Prefix.setPLayerPrefix(player, "&7[&9Polizei&7] ");
+                Prefix.setPLayerPrefix(player, "&7[&9Polizei&7] ");
                 break;
             case "default":
-                commandSender.sendMessage(Prefix.errorPrefix() + "Dieser Tag existiert nicht! Nutze /status list um die verfügbaren dir anzuzeigen");
+                commandSender.sendMessage("Dieser Tag existiert nicht! Nutze /status list um die verfügbaren dir anzuzeigen");
                 break;
             case "list":
-                player.sendMessage(Prefix.PluginPrefix() + ChatColor.DARK_AQUA + "Verfügbare Status Prefixe: " + ChatColor.GOLD + "ghg, bambus, redstone, live, afk, pvp, builder, troll, polizei");
+                player.sendMessage( ChatColor.DARK_AQUA + "Verfügbare Status Prefixe: " + ChatColor.GOLD + "ghg, bambus, redstone, live, afk, pvp, builder, troll, polizei");
                 break;
         }
         if (strings[0].equals("list")) return true;
         player.sendMessage(ChatColor.DARK_AQUA + "Dein Status ist nun " + ChatColor.GOLD + strings[0]);
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (args.length == 1) {
+            String partial = args[0].toLowerCase();
+            List<String> suggestions = new ArrayList<>();
+            for (String option : STATUS_OPTIONS) {
+                if (option.startsWith(partial)) {
+                    suggestions.add(option);
+                }
+            }
+            return suggestions;
+        }
+        return null;
     }
 }
