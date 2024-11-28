75655aa8af97ad600b24b57622962b1f3f82c3c9
construktdev <construktai@gmail.com>
2024-11-28 21:25:02 +0100

    Fix world start confirmation status update
    The `isStarted` flag was not set correctly following the start confirmation, potentially leading to incorrect application behavior. This commit ensures that the preparation start command's status is accurately updated after confirming the world start.

--- src/main/java/de/construkter/glitzoriumSMP/release/ConfirmStart.java
+++ src/main/java/de/construkter/glitzoriumSMP/release/ConfirmStart.java
@@ -8,13 +8,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ConfirmStart extends Utils implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (commandSender.hasPermission("glitzorium.smp.start")) {
             for (Player p : Objects.requireNonNull(commandSender.getServer().getOnlinePlayers())) {
                 if (!(p.hasPermission("glitzorium.smp.prepare.gamemode"))) {
@@ -29,6 +30,7 @@ public class ConfirmStart extends Utils implements CommandExecutor {
             Objects.requireNonNull(commandSender.getServer().getWorld("world")).getWorldBorder().setSize(20000);
             PrepareStartCommand.isStarted = false;
             commandSender.sendMessage(ChatColor.GREEN + "Fully started!");
+            PrepareStartCommand.isStarted = true;
             return true;
         }
         commandSender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
