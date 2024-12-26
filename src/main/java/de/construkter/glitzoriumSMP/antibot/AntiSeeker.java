2818174cd141aa0923a0ed5dbae082b81571a9e1
construktdev <construktai@gmail.com>
2024-12-26 16:39:02 +0100

    Add Cracked Account Protection

--- src/main/java/de/construkter/glitzoriumSMP/antibot/AntiSeeker.java
+++ src/main/java/de/construkter/glitzoriumSMP/antibot/AntiSeeker.java
@@ -9,15 +9,15 @@ import org.bukkit.event.player.PlayerLoginEvent;
 import java.util.Objects;
 
 public class AntiSeeker implements Listener {
-    String[] seekersIp = {"154.213."};
+    String[] seekersIp = {"8.53.150.111"};
     String[] seekersName = {"server", "seeker", "finder"};
 
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         for (String adress : seekersIp) {
-            if (Objects.requireNonNull(event.getPlayer().getAddress()).toString().startsWith( adress)) {
-                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.RED + "Deine IP Adresse ist aus Verdacht auf einen Bot/Seeker gesperrt.");
-                GlitzoriumSMP.sendMessage("Bot Protection", "IP Adresse: " + event.getPlayer().getAddress().toString() + " (" + event.getPlayer().getName() + ") wurde gesperrt.\nGrund: Verdacht auf Bot/Seeker");
+            if (!Objects.requireNonNull(event.getPlayer().getAddress()).getAddress().getHostAddress().equals(adress)) {
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.RED + "Bitte nutze den offiziellen glitzorium.de Proxy");
+                GlitzoriumSMP.sendMessage("Proxy Protection", event.getPlayer().getName() + " veruschte über andere Proxies zu joinen");
             }
         }
         for (String name : seekersName) {
